8fd2613c1d7338327e15659d76819cfd8971fdee
mitsuaki-s <2201145003dy@cyber-u.ac.jp>
2025-06-07 12:58:58 +0900

    add hello page

---
+++ routes/hello.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+fouter.get('/', function(req, res, next) {
+  res.render('hello', { title: 'Hello Express' });
+});
+
+module.exports = touter;
