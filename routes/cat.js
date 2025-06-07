6365a60e6c4bda9ba5762ffdf8f231a66c90b9df
mitsuaki-s <2201145003dy@cyber-u.ac.jp>
2025-06-08 01:32:53 +0900

    update file

--- routes/cat.js
+++ routes/cat.js
@@ -1,16 +1,15 @@
-const express = require('express');
-const router = express.Router();
+var express = require('express');
+var router = express.Router();'
 const request = require('request');
 
 router.get('/', async (req, res) => {
-  request('https://api.thecatapi.com/v1/images/search', function (error, response, body) {
-    if (!error && response.statusCode === 200) {
-      const data = JSON.parse(body);
-      res.json(data);
-    } else {
-      res.status(500).send('Failed to fetch cat image');
-    }
-  });
+request('https://api.thecatapi.com/v1/images/search', function (error, response,
+body) {
+if (!error && response.statusCode == 200) {
+const data = JSON.parse(body);
+res.json(data);
+}
 });
+})
 
 module.exports = router;
